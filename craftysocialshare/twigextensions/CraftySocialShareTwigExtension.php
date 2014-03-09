075c90eeea5653e9e79eb48f6ff32bdc3c961ffe
Jake Chapman <jake@imjakechapman.com>
2014-03-09 14:50:35 -0700

    Updated plugin to have better control of outputted buttons.

--- craftysocialshare/twigextensions/CraftySocialShareTwigExtension.php
+++ craftysocialshare/twigextensions/CraftySocialShareTwigExtension.php
@@ -1,6 +1,14 @@
 <?php
 namespace Craft;
 
+// Define our script instead of rewriting them everywhere
+define("FACEBOOK_SCRIPT", "<div id=\"fb-root\"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = \"//connect.facebook.net/en_US/all.js#xfbml=1&appId=543069402473732\"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>");
+
+define("TWITTER_SCRIPT", "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"https://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>");
+
+define("GOOGLE_SCRIPT", "<script type=\"text/javascript\">(function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/platform.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); </script>");
+
+
 class CraftySocialShareTwigExtension extends \Twig_Extension
 {
   public function getName()
@@ -17,61 +25,90 @@ class CraftySocialShareTwigExtension extends \Twig_Extension
     );
   }
 
-  public function getSocialShareScripts($opts = null)
+
+  /*
+   * @param
+   * $scripts(string) - pipe delimited list of scripts to include
+   *
+   * @return
+   * $scripts(string) = echos appropriate script tags
+   */
+  public function getSocialShareScripts($scriptList = null)
   {
-    switch ($opts) {
-      case 'facebook':
-        // Facebook Scripts
-        $scripts = "<div id=\"fb-root\"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = \"//connect.facebook.net/en_US/all.js#xfbml=1&appId=543069402473732\"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>";
-        break;
 
-      case 'twitter':
-        // Twitter scripts
-        $scripts = "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"https://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>";
-        break;
+    $scripts; // scripts to echo
 
-      case 'google':
-        $scripts  = "<script type=\"text/javascript\">";
-        $scripts .= "  (function() {";
-        $scripts .= "    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;";
-        $scripts .= "    po.src = 'https://apis.google.com/js/platform.js';";
-        $scripts .= "    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);";
-        $scripts .= "  })();";
-        $scripts .= "</script>";
-        break;
-      
-      default:
-        $scripts = "<div id=\"fb-root\"></div><script>(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = \"//connect.facebook.net/en_US/all.js#xfbml=1&appId=543069402473732\"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));</script>";
-        $scripts .= "\r";
-        $scripts .= "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\"https://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);}}(document,\"script\",\"twitter-wjs\");</script>";
-        $scripts .= "\r";
-        $scripts .= "<script type=\"text/javascript\">(function() {var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true; po.src = 'https://apis.google.com/js/platform.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s); })(); </script>";
-        break;
-    }
+    if ( $scriptList != null ) {
+      $scriptList = explode('|', $scriptList); // pipe delimited list of scripts to include
+
+      foreach ($scriptList as $script) {
+        if ( $script == 'facebook' ) {
+
+          $scripts .= FACEBOOK_SCRIPT;
+
+        } elseif ( $script == 'twitter' ) {
+
+          $scripts .= TWITTER_SCRIPT;
+
+        } elseif ( $script == 'google' ) {
+
+          $scripts .= GOOGLE_SCRIPT;
+
+        }
+      } // end foreach
+
+    } else {
+       $scripts .= FACEBOOK_SCRIPT;
+       $scripts .= TWITTER_SCRIPT;
+       $scripts .= GOOGLE_SCRIPT;
+    } // end if
 
     echo $scripts;
   }
 
 
-  public function getSocialBtn($type = null, $url = null, $width = '100px') {
-    
-    if ( !$url ) {
-      // Get Current URL
-      $url = craft()->request->getHostInfo();
-      $url .= craft()->request->getUrl();
-    }
+  /*
+   * @param
+   * $type(string, required) - Type of button to display
+   * $url(string, optional) - URL to use for button (default is current request url)
+   * $opts(array, optional) - override parameters for button data attributes
+   *
+   * @return
+   * (string) - echos appropriate button markup
+   */
+  public function getSocialBtn($type = null, $opts = null) {
+
+    $url = $this->checkOpt($opts['url'], craft()->request->getHostInfo() . craft()->request->getUrl());
 
     switch ($type) {
       case 'facebook':
-        $button = "<div class=\"fb-like\" data-href=".$url." data-width=".$width." data-layout=\"button_count\" data-action=\"like\" data-show-faces=\"false\" data-share=\"false\"></div>";
+
+        if ( $opts !== null ) {
+
+          $button = "<div class=\"fb-like\" data-href=". $url ." data-width=\"{$this->checkOpt($opts["width"], '100px')}\" data-layout=\"{$this->checkOpt($opts['layout'], 'button')}\" data-action=\"{$this->checkOpt($opts["action"], 'like')}\" data-show-faces=\"{$this->checkOpt($opts['faces'], 'false')}\" data-share=\"{$this->checkOpt($opts['share'], 'false')}\"></div>";
+
+        } else {
+          $button = "<div class=\"fb-like\" data-href=". $url ." data-width=\"100px\" data-layout=\"button_count\" data-action=\"like\" data-show-faces=\"false\" data-share=\"false\"></div>";
+        }
+
         break;
 
       case 'twitter':
-        $button = "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-size=".$width." data-url=". $url ." data-lang=\"en\">Tweet</a>";
+
+        if ( $opts !== null ) {
+          $button = "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-size=\"{$this->checkOpt($opts["size"], '100px')}\" data-via=\"{$this->checkOpt($opts["via"], '')}\" data-url=\"{$this->checkOpt($opts["url"], $url)}\" data-lang=\"{$this->checkOpt($opts["lang"], 'en')}\" data-text=\"{$this->checkOpt($opts["text"], '')}\" data-related=\"{$this->checkOpt($opts["related"], '')}\" data-count=\"{$this->checkOpt($opts["count"], 'none')}\" data-counturl=\"{$this->checkOpt($opts["counturl"], $url)}\" data-hashtags=\"{$this->checkOpt($opts["hashtags"], "")}\" data-dnt=\"{$this->checkOpt($opts["opt-out"], "false")}\">Tweet</a>";
+        } else {
+          $button = "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-size=\"100px\" data-url=". $url ." data-lang=\"en\">Tweet</a>";
+        }
+
         break;
 
       case 'google':
-        $button = "<div class=\"g-plusone\" data-annotation=\"inline\" data-width=".$width."></div>";
+        if ( $opts !== null ) {
+          $button = "<div class=\"g-plusone\" data-href=\"{$this->checkOpt($opts["href"], $url)}\" data-size=\"{$this->checkOpt($opts["size"], 'standard')}\" data-annotation=\"{$this->checkOpt($opts["annotation"], 'bubble')}\" data-width=\"{$this->checkOpt($opts["width"], '120')}\" data-align=\"{$this->checkOpt($opts["align"], 'left')}\" expandTo=\"{$this->checkOpt($opts["expandTo"], '')}\" data-recommendations=\"{$this->checkOpt($opts["recommendations"], 'true')}\" data-count=\"{$this->checkOpt($opts["count"], 'true')}\"></div>";
+        } else {
+          $button = "<div class=\"g-plusone\" data-annotation=\"inline\" data-width=\"120px\"></div>";
+        }
         break;
       
       default:
@@ -83,18 +120,59 @@ class CraftySocialShareTwigExtension extends \Twig_Extension
   }
 
 
-  public function getSocialBtns($width = '120')
+  /*
+   * @param
+   * $type(string) - Type of button to display
+   * $url(string) - URL to use for button (default is current request url)
+   * $width(string) - Width in pixels for button to be
+   *
+   * @return
+   * $button(string) - echos appropriate button markup
+   */
+  public function getSocialBtns($btnList = null, $width = '120')
   {
-    // Get Current URL
-    $url = craft()->request->getHostInfo();
-    $url .= craft()->request->getUrl();
+    $buttons; // our return list of markup
+    $btnList = explode('|', $btnList); // pipe delimited list of buttons to include
+    $url = craft()->request->getHostInfo() . craft()->request->getUrl(); // Get Current URL
 
-    $buttons = "<div class=\"fb-like\" data-href=". $url ." data-width=". $width ." data-layout=\"button_count\" data-action=\"like\" data-show-faces=\"false\" data-share=\"false\"></div>";
-    $buttons .= "\r";
-    $buttons .= "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-url=". $url ." data-lang=\"en\"></a>";
-    $buttons .= "\r";
-    $buttons .= "<div class=\"g-plusone\" data-annotation=\"inline\" data-width=\"120\"></div>";
+    if ( $btnList != null ) {
+      foreach ($btnList as $btn) {
+        
+        if ( $btn == 'facebook' ) {
+
+          $buttons .= "<div class=\"fb-like\" data-href=". $url ." data-width=". $width ." data-layout=\"button_count\" data-action=\"like\" data-show-faces=\"false\" data-share=\"false\"></div>";
+
+        } elseif ( $btn == 'twitter' ) {
+
+          $buttons .= "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-url=". $url ." data-lang=\"en\"></a>";
+
+        } elseif ( $btn == 'google' ) {
+
+          $buttons .= "<div class=\"g-plusone\" data-annotation=\"inline\" data-width=\"120\"></div>";
+
+        } else {
+          $buttons = "<div class=\"fb-like\" data-href=". $url ." data-width=". $width ." data-layout=\"button_count\" data-action=\"like\" data-show-faces=\"false\" data-share=\"false\"></div>";
+          $buttons .= "\r";
+          $buttons .= "<a href=\"https://twitter.com/share\" class=\"twitter-share-button\" data-url=". $url ." data-lang=\"en\"></a>";
+          $buttons .= "\r";
+          $buttons .= "<div class=\"g-plusone\" data-annotation=\"inline\" data-width=\"120\"></div>";
+        }
+
+      }
+    }
 
     echo $buttons;
   }
+
+
+
+  public function checkOpt($var, $fallback) {
+    if ( $var == null ) {
+      return $fallback;
+    } else {
+      return $var;
+    }
+  }
+
+
 }
